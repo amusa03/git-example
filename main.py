896b6abdc42ca1c0e9cbf6182c9c9535a76a0286
amusa03 <abdurrahman.musa@ada.ac.uk>
2022-11-04 12:27:56 +0000

    Guess game

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import random
+
+
+num = 0
+
+def generate_random_number():
+  return (random.randint(0,100))
+
+def difference_from_answer(guess, answer = generate_random_number()):
+  if guess < answer:
+      print ("Too low")
+  elif guess > answer:
+      print("Too high")
+  else:
+    print("Correct")
+   
+      
+    
+difference_from_answer(67)
+  
